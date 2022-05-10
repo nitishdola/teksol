0882f1badb68476336dad4926268859977a389fc
Nitish Dolakasharia <nitish.dola@gmail.com>
2022-05-10 11:29:44 +0530

    handle click

--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 export const Navbar = () => {
 
-    handleClick = (e) => {
+    const handleClick = (e) => {
         e.preventDefault();
       };
 
